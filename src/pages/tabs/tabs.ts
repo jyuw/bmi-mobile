64d6dceeecc9738e6f7fd754188db620bf623d3d
Jade <jywfang@gmail.com>
2018-03-06 09:59:33 +0100

    initial commit removed some scaffolded pages

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { AboutPage } from '../about/about';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab2Root = AboutPage;
+
+  constructor() {
+
+  }
+}
