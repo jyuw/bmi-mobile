c8be7faa13f80dfa124888b4413c10e87f60e974
Jade <jywfang@gmail.com>
2018-03-06 15:03:04 +0100

    added imperial selector defaults on metric page

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -1,40 +1,46 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CalculatorPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-calculator',
   templateUrl: 'calculator.html',
 })
+
 export class CalculatorPage {
-  height: number;
-  weight: number;
+  units: string = "metric";
+  heightMetric: number;
+  weightMetric: number;
+  heightImperial: number;
+  weightImperial: number;
+
   bmiValue: number;
   bmiMessage: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  calculateBMI(){
-    if (this.weight > 0 && this.height > 0) {
-      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
+  calculateMetricBMI() {
+    if (this.weightMetric > 0 && this.heightMetric > 0) {
+      let finalBmi = this.weightMetric / (this.heightMetric / 100 * this.heightMetric / 100);
       this.bmiValue = parseFloat(finalBmi.toFixed(2));
       this.setBMIMessage();
     }
   }
 
-  ionViewDidLoad(){
+  calculateImperialBMI() {
+    if (this.weightImperial > 0 && this.heightImperial > 0) {
+      let finalBmi = (this.weightImperial * 703) / (this.heightImperial * this.heightImperial);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+  }
+
+  ionViewDidLoad() {
     console.log('ionViewDidLoad CalculatorPage');
   }
 
-  private setBMIMessage(){
+  private setBMIMessage() {
     if (this.bmiValue < 18.5) {
       this.bmiMessage = "Underweight"
     }
